04371f2ad3c92fa857b4618afe1aace03d9ac8ca
Sriram J. Sathish <sriram.sathish@gmail.com>
2013-07-12 07:15:49 +0000

    Updated web.js to read from file

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  var buffer = fs.readFileSync('index.html');
+  var str = buffer.toString('utf-8');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
